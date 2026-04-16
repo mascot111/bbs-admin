051d0ce269410414434bf8104a984af7646cf53f
Luciddreamz <mascotahenkorah192@gmail.com>
2026-04-16 11:03:36 +0000

    ju

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Replace these with the keys from your Supabase dashboard
 const supabaseUrl = 'https://rcdqkcyavilgqcxaaliu.supabase.co';
-const supabaseKey = 'sb_publishable_wcy7n0OVf_D9uFFD8HBqrQ_zUiylVcA';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjZHFrY3lhdmlsZ3FjeGFhbGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NTE5ODEsImV4cCI6MjA5MTMyNzk4MX0.3EjiQxpc3VOXois-I7ASfueq9vU-7QeElSUM4sKAHK8';
 
 // Initialize the Supabase Engine
 export const supabase = createClient(supabaseUrl, supabaseKey);
